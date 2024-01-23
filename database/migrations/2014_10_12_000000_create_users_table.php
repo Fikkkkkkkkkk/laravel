3887bf55dd44d15860ad8af1014823279cf1441f
fikri <mhdfikrishr@gmail.com>
2024-01-23 14:42:28 +0800

    complete login and registration form connected with database. but not apply styles yet

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('compName');
+            $table->string('currentJob');
+            $table->date('yearBorn');
+            $table->integer('fulltimeExp');
+            $table->integer('ExpData');
+            $table->string('descBigdata');
+            $table->string('orgFocus');
+            $table->string('areaFocus');
             $table->string('password');
+            $table->string('confirmPassword');
+            $table->string('secQuestion');
+            $table->string('secAnswer');
+            
+            $table->timestamp('email_verified_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
